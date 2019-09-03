14447c4c78189edf5d684be776fbcf203964a6cd
ian-sanchez <silkyhairbear@gmail.com>
2019-09-03 16:19:54 -0700

    styling

--- react-github-user-card/src/components/Card.js
+++ react-github-user-card/src/components/Card.js
@@ -4,8 +4,10 @@ function UserCard(props) {
     return (
         <div className='Card'>
             <h2>{props.user.login}</h2>
+            <img src='../../sponge.png' alt='pic'/>
             <p>{props.user.url}</p>
             <div>
+                <h2 className='total'>Followers:</h2>
                 {props.followers.map(follower => (
                     <div key={follower.id}>{follower.login}</div>
                 ))}
